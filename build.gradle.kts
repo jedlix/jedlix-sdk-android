1661b554fb6ff401ef9d53df053531268fca6e92
Jedlix <info@jedlix.com>
2025-06-10 19:06:31 +0000

    Version 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,14 +26,13 @@ val signingSecretKeyRingFile: String? by project
 val sonatypeCentralUserToken: String? by project
 val sonatypeCentralUserTokenPassword: String? by project
 
-val sonatypeStagingProfileId: String? by project
 
 // Stub secrets to let the project sync and build without the publication values set up
 ext["signing.keyId"] = signingKeyId ?: System.getenv("SIGNING_KEY_ID")
 ext["signing.password"] = signingPassword ?: System.getenv("SIGNING_PASSWORD")
 ext["signing.secretKeyRingFile"] = signingSecretKeyRingFile ?: System.getenv("SIGNING_SECRET_KEY_RING_FILE")
 
-ext["sonatypeStagingProfileId"] = sonatypeStagingProfileId ?: System.getenv("SONATYPE_STAGING_PROFILE_ID")
+group = "com.jedlix"
 
 nexusPublishing {
     repositories {
@@ -42,7 +41,6 @@ nexusPublishing {
             snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
             username.set(sonatypeCentralUserToken)
             password.set(sonatypeCentralUserTokenPassword)
-            stagingProfileId.set(sonatypeStagingProfileId)
         }
     }
 }
